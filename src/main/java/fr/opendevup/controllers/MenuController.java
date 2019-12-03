a323c263199779975fb7e5c4d66c0e91ef580f02
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-12-03 18:35:44 +0100

    typo

---
+++ src/main/java/fr/opendevup/controllers/MenuController.java
@@ -0,0 +1,20 @@
+package fr.opendevup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class MenuController {
+	
+	@RequestMapping(value="pages/menu",method = RequestMethod.GET)
+	public String  accueil(Model model,@RequestParam(required = false, defaultValue = "----------") String nom)
+	{
+		model.addAttribute("nom",nom);
+		return "pages/menu";
+	}
+	
+
+}
+
