2378be57172215fd8e3399f534e0be2bee41f5ed
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-12-03 19:05:01 +0100

    typo

--- src/main/java/fr/opendevup/controllers/AccueilController.java
+++ src/main/java/fr/opendevup/controllers/AccueilController.java
@@ -1,17 +1,15 @@
 package fr.opendevup.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AccueilController {
 	
 	@RequestMapping(value="/",method = RequestMethod.GET)
-	public String  accueil(Model model,@RequestParam(required = false, defaultValue = "----------") String nom)
+	public String  accueil()
 	{
-		model.addAttribute("nom",nom);
+		
 		return "/index";
 	}
 	
