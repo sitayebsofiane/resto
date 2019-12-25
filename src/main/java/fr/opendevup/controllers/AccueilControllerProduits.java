83cddf8b32586a5cfca92775e1a2d7f6343b6bde
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-12-25 11:58:38 +0100

    update

---
+++ src/main/java/fr/opendevup/controllers/AccueilControllerProduits.java
@@ -0,0 +1,21 @@
+package fr.opendevup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AccueilControllerProduits {
+	@RequestMapping(value = "pages/produits",method = RequestMethod.GET)
+	public String AccueilProduits() {
+		
+		return "pages/produits";
+	
+	}
+	@RequestMapping(value = "pages/menus",method = RequestMethod.GET)
+	public String AccueilMenus() {
+		
+		return "pages/menus";
+	
+	}
+}
