bcfddc2a295c040d9deada394b0674a5a2a6e372
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-01-26 19:36:10 +0100

    fixation du controller menu

--- src/main/java/fr/opendevup/controllers/AccueilControllerProduits.java
+++ src/main/java/fr/opendevup/controllers/AccueilControllerProduits.java
@@ -11,15 +11,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import fr.opendevup.dao.MenuRepository;
 import fr.opendevup.dao.ProduitRepository;
 import fr.opendevup.entities.Client;
 import fr.opendevup.entities.Produit;
+import fr.opendevup.entities.Menu;
 
 @Controller
 @SessionAttributes({"client"})
 public class AccueilControllerProduits {
+	
 	@Autowired
 	private ProduitRepository produitRepo;
+	
+	@Autowired
+	private MenuRepository menuRepo;
+	
 	
 	
 	@ModelAttribute("client")
@@ -44,10 +51,10 @@ public class AccueilControllerProduits {
 	@RequestMapping(value = "pages/menus",method = RequestMethod.GET)
 	public String accueilMenus(Model model,@RequestParam(name="page", defaultValue="0")int page,
 			@RequestParam(name="size", defaultValue="6")int size) {
-		Page<Produit> produits= produitRepo.findAll(PageRequest.of(page,size));
-		model.addAttribute("listeProduit",produits.getContent());
+		Page<Menu> menus= menuRepo.findAll(PageRequest.of(page,size));
+		model.addAttribute("listeProduit",menus.getContent());
 		//creation d'un tableu de page
-		int [] pages= new int [produits.getTotalPages()];
+		int [] pages= new int [menus.getTotalPages()];
 		model.addAttribute("pages",pages);
 		model.addAttribute("size",size);
 		model.addAttribute("pageCourante",page);
