d71182e2620e6b2e324ed36005ebc0fae8fda83f
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-01-26 19:27:28 +0100

    ajout menuRepository

---
+++ src/main/java/fr/opendevup/dao/MenuRepository.java
@@ -0,0 +1,14 @@
+package fr.opendevup.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import fr.opendevup.entities.Produit;
+
+public interface MenuRepository extends JpaRepository<Produit, Integer>{
+	@Query("select p from Produit p where p.nom like :x ")
+	public Page<Produit> chercher(@Param("x")String mc,Pageable pageable);
+}
---
+++ src/main/java/fr/opendevup/entities/Menu.java
@@ -0,0 +1,95 @@
+package fr.opendevup.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Menu implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idMenu;
+	
+	@NotNull
+	@Size(min = 2, max = 30)
+	private String nom;
+	
+	@NotNull
+	@Size(min = 2)
+	private String description;
+	
+	private double prix;
+
+	private int quantite;
+
+	public Menu() {
+		super();
+	}
+
+	public Menu(String nom, String description, double prix, int quantite) {
+		this.setNom(nom);
+		;
+		this.setDescription(description);
+		;
+		this.setPrix(prix);
+		this.setQuantite(quantite);
+	}
+
+	public int getIdProduit() {
+		return idMenu;
+	}
+
+	public void setIdProduit(int id) {
+		this.idMenu = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	@Override
+	public String toString() {
+		return "Produit [idProduit=" + idMenu + ", nom=" + nom + ", description=" + description + ", prix=" + prix
+				+ "€" + ", quantité=" + quantite + "]";
+	}
+
+}
