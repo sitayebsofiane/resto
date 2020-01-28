4cc8936ebf90993fd48729b1d417467ff81dae7a
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-01-28 21:48:05 +0100

    retour sur la version precdente

--- src/main/java/fr/opendevup/entities/Menu.java
+++ src/main/java/fr/opendevup/entities/Menu.java
@@ -1,17 +1,106 @@
 package fr.opendevup.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
-@PrimaryKeyJoinColumn(name = "idProduit")
-public class Menu extends Produit {
+public class Menu implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idMenu;
+	@NotNull
+	@Size(min = 2, max = 30)
+	private String nom;
+	
+	@NotNull
+	@Size(min = 2)
+	private String description;
+	
+	private double prix;
 
+	private int quantite;
+	
+	
+
+	public Menu() {
+		super();
+		
+	}
+	 
+	
+	public int getIdMenu() {
+		return idMenu;
+	}
+
+
+	public void setIdMenu(int idMenu) {
+		this.idMenu = idMenu;
+	}
+
+
+	public Menu(@NotNull @Size(min = 2, max = 30) String nom, @NotNull @Size(min = 2) String description, double prix,
+			int quantite) {
+		this.nom = nom;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Menu [idMenu=" + idMenu + ", nom=" + nom + ", description=" + description + ", prix=" + prix
+				+ ", quantite=" + quantite + "]";
+	}
+
+
+	
+	
 	
 	
 }
--- src/main/java/fr/opendevup/entities/Produit.java
+++ src/main/java/fr/opendevup/entities/Produit.java
@@ -6,13 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
 public class Produit implements Serializable {
 
 	private static final long serialVersionUID = 1L;
