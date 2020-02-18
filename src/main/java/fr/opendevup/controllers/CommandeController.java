2512b69221882e3823db009a6b2431a3bbd8630f
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-18 12:27:38 +0100

    update

--- src/main/java/fr/opendevup/controllers/CommandeController.java
+++ src/main/java/fr/opendevup/controllers/CommandeController.java
@@ -130,19 +130,32 @@ public class CommandeController {
 				client.getNom(), client.getAdresse(),client.getTelephone(),0));
 		return "redirect:/";
 	}
+	
 	@RequestMapping(value = "/admin/traiterCommande",method = RequestMethod.GET)
 	public String traiterCommande(int idCommande,String mc,int page,int size,int idClient) {
+		//je declare une variable pour enrigistré idClientProduitCommande
+		int  idClientProduitCommande = 0;
 		//je selectione tout les produits du panier
 		List<ClientProduitCommande> produitPanier=  consulterCommandeRepo.findAll();
-		/*
-		 * apres le traitement de la commande je mets le statut a 1
-		 */
-		for (ClientProduitCommande clientProduitCommande : produitPanier) 
-			if(idClient == clientProduitCommande.getIdClient())
-				//consulterCommandeRepo.deleteById(clientProduitCommande.getIdClientProduitCommande());
-				consulterCommandeRepo.changerStatut(idClient);
-		//commandeRepo.deleteById(idCommande);
-			commandeRepo.changerStatut(idClient);
+		// apres le traitement de la commande je mets le statut a 1
+		for (ClientProduitCommande clientProduitCommande : produitPanier) {
+			idClientProduitCommande = clientProduitCommande.getIdClientProduitCommande();
+			if(idClient == clientProduitCommande.getIdClient()) {
+				//je creer un nouveau produit dans la table Client ProduitCommande avec les même caratiristique 
+				Produit produit = produitRepo.getOne(clientProduitCommande.getIdProduit());
+				//je suprime le produit dans la table ClientProduitCommande
+				consulterCommandeRepo.deleteById(idClientProduitCommande);
+				//j'enrigistre ce produit dans la table avec le statut a 1
+				consulterCommandeRepo.save(new ClientProduitCommande(idClient, produit.getIdProduit(), produit.getNom(), 
+						produit.getDescription(), produit.getPrix(), 1));
+				}
+			}
+			//je creer une nouvelle commande dans la table Commande avec les même caratiristique 
+			Commande commande = commandeRepo.getOne(idCommande);
+			commandeRepo.deleteById(idCommande);
+			commandeRepo.save(new Commande(commande.getDate(),commande.getPrixTotal(), idClient,
+					commande.getNomClient(), commande.getAdresseClient(), commande.getTelephoneClient(), 1));
+			
 		return "redirect:/admin/commandes?page="+page+"&size="+size+"&mc="+mc;
 	}
 	
--- src/main/java/fr/opendevup/dao/CommandeRepository.java
+++ src/main/java/fr/opendevup/dao/CommandeRepository.java
@@ -3,16 +3,18 @@ package fr.opendevup.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import org.springframework.stereotype.Repository;
 
 import fr.opendevup.entities.Commande;
-
+@Repository
 public interface CommandeRepository extends JpaRepository<Commande, Integer> {
 	@Query("select c from Commande c where c.nomClient like :x and c.statut = 0")
 	public Page<Commande> chercher(@Param("x")String mc,Pageable pageable);
-
-	@Query("update Commande c set  c.statut = 1 where c.idClient = :x ")
-	public void changerStatut(@Param("x")int idClient);
+	
+	@Modifying
+	@Query("UPDATE Commande c SET c.statut = 1 WHERE c.idCommande = :x")
+	public void changerStatut(@Param("x")int idCommande);
 }
