962f406444d2818172ea371e7e3b2e36b7a4d73e
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-25 20:08:48 +0100

    ajout diapo

--- src/main/java/fr/opendevup/security/SecurityConfig.java
+++ src/main/java/fr/opendevup/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
     	http.formLogin().loginPage("/login");
     	http.authorizeRequests().antMatchers("/","/admin/ajouterClients","/admin/enregistrerClient","/pages/*").permitAll();
-    	http.authorizeRequests().antMatchers("/admin","/admin/consulterProduits","/admin/clients","/admin/commandes").hasRole("USER");
+    	http.authorizeRequests().antMatchers("admin","/admin/consulterProduits","/admin/clients","/admin/commandes").hasRole("USER");
     	http.authorizeRequests().antMatchers("admin","/admin/*").hasRole("ADMIN");
     	http.exceptionHandling().accessDeniedPage("/403");
     	
